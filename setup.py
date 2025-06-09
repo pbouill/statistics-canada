3549d640e6b30f9173ce1f9ebfaeecaed2d9452f
Paul Bouillon <paul.bouillon@hatch.com>
2025-06-09 14:48:39 -0400

    Refactor build process by replacing custom build script with 'python -m build' and updating setup.py for improved path handling.

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
+import sys
+from pathlib import Path
+from contextlib import contextmanager
+
 import setuptools
-from package_info import VersionInfo
+
+@contextmanager
+def local_syspath():
+    local_path = Path(__file__).parent
+    original_path = sys.path.copy()
+    try:
+        if str(local_path) not in sys.path:
+            sys.path.insert(0, str(local_path))
+        yield
+    finally:
+        sys.path = original_path
 
 
-current_version = VersionInfo.get_latest()
+with local_syspath():
+    from package_info import VersionInfo
+    current_version = VersionInfo.get_latest()
 
 setuptools.setup(
     package_dir={current_version.package_name: current_version.package_name},
